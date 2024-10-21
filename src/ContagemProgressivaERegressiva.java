96bc4ca952d4c1190c4ca649edc1137c17ecb3cd
Ytalo <ytalo986.x@gmail.com>
2024-10-21 17:59:24 -0300

    Contagem progressiva/regressiva

--- src/ContagemProgressivaERegressiva.java
+++ src/ContagemProgressivaERegressiva.java
@@ -1,47 +1,43 @@
 import java.util.Scanner;
     public class ContagemProgressivaERegressiva {
     public static void main(String[] args) {
-    Scanner entrada = new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in);
 
-        // Sistema de entrada de valores [SEV]
+        String resp;
+        do {
+            // Sistema de entrada de valores [SEV]
 
-        System.out.println("Digite o numero que voce quer comecar a contagem: ");
-        int continic = entrada.nextInt();
-        System.out.println("Digite em que numero voce pretende parar a contagem: ");
-        int contfim = entrada.nextInt();
+            System.out.println("Digite o numero que voce quer comecar a contagem: ");
+            int continic = entrada.nextInt();
+            System.out.println("Digite em que numero voce pretende parar a contagem: ");
+            int contfim = entrada.nextInt();
 
-        //Contagem Progressiva
+            //Contagem Progressiva
 
-        if (continic < contfim){
-            System.out.println("-------------------");
-            System.out.println(continic);
-            while (continic < contfim){
-                int c = ++continic;
-                System.out.println(c);
+            if (continic < contfim) {
+                System.out.println("-------------------");
+                System.out.println(continic);
+                while (continic < contfim) {
+                    int c = ++continic;
+                    System.out.println(c);
+                }
+
+                // Contagem Regressiva
+
+            } else if (continic > contfim) {
+                System.out.println("-------------------");
+                System.out.println(continic);
+                while (continic > contfim) {
+                    int c = --continic;
+                    System.out.println(c);
+                }
             }
 
-            // Contagem Regressiva
+            // Deseja Repetir o processo de contagem regressiva/progressiva
 
-        }else if (continic > contfim){
-            System.out.println("-------------------");
-            System.out.println(continic);
-            while (continic > contfim){
-                int c = --continic;
-                System.out.println(c);
-            }
-        }
-
-
-
-
-
-
-
-
-
-
-
-
+            System.out.println("Voce deseja continuar fazendo contagens [s/n] ");
+            resp = entrada.next();
+        } while (resp.equals("s"));
 
 
     }
