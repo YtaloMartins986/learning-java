06461ac370eef348a97cf26469e21f5b1701ce47
Ytalo <ytalo986.x@gmail.com>
2024-10-22 09:27:23 -0300

    Estrutura de repeticao for, {pratica}

---
+++ src/ContagemPulando10N.java
@@ -0,0 +1,18 @@
+public class ContagemPulando10N {
+public static void main(String[] args){
+
+    for (int c=0;c<=100;c+=10){
+        if (c == 0){
+            continue;
+        }
+        System.out.println(c);
+
+    }
+
+
+
+
+
+
+   }
+ }
