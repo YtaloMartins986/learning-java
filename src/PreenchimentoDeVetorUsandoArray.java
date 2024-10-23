e9eb90c7d142257c59c32df381f87fd2d17bacc6
Ytalo <ytalo986.x@gmail.com>
2024-10-23 10:33:54 -0300

    Preenchimento de vetor usando array

--- src/PreenchimentoDeVetorUsandoArray.java
+++ src/PreenchimentoDeVetorUsandoArray.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
+import java.util.Arrays;
 public class PreenchimentoDeVetorUsandoArray {
+    public static void main(String[] args) {
+        //vetor com 20 espacos
+        int[] valores = new int [20];
+        //preenchimento de todos os 20 espacos do vetor automatico/eficiente
+        Arrays.fill(valores,0);
+        //for it, para mostrar todos os valores que estao no vetor
+        for (int valor: valores){
+            System.out.print(valor+ " ");
+        }
+    }
 }
+
