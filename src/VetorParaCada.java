c60c4b237797f6c71fbf0afc3a5c326fa0812ed8
Ytalo <ytalo986.x@gmail.com>
2024-10-23 09:56:23 -0300

    Vetor para cada

--- src/VetorParaCada.java
+++ src/VetorParaCada.java
@@ -1,2 +1,12 @@
 public class VetorParaCada {
-}
+public static void main(String[] args){
+    double[] x = {3.4,-2.3,5.6};
+    for (double valor: x){
+        System.out.print(valor + "  ");
+
+    }
+
+
+    }
+ }
+
