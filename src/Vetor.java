2cbd25d7bb87f10a6a9c0553c04ce328d2ad8386
Ytalo <ytalo986.x@gmail.com>
2024-10-23 09:56:07 -0300

    Vetor

--- src/Vetor.java
+++ src/Vetor.java
@@ -1,2 +1,10 @@
 public class Vetor {
-}
+public static void main(String[] args){
+    int[] v = {1,3,4,7};
+    System.out.println("O total de casas de V "+v.length);
+        for (int c=0;c<=v.length-1;c++){
+            System.out.println("na posicao "+c+" temos o valor "+v[c]);
+        }
+
+    }
+ }
