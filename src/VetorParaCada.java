f335c413f27ecd34d63b731a3d802157acb4aeb4
Ytalo <ytalo986.x@gmail.com>
2024-10-23 10:32:59 -0300

    Vetor e for it

--- src/VetorParaCada.java
+++ src/VetorParaCada.java
@@ -1,8 +1,17 @@
+import java.util.Arrays;
+
 public class VetorParaCada {
 public static void main(String[] args){
     double[] x = {3.4,-2.3,5.6};
+
+    //*deixando o vetor em ordem numerica
+
+    Arrays.sort(x);
+
+    // estrutura for para cada
+
     for (double valor: x){
-        System.out.print(valor + "  ");
+        System.out.print(valor + " ");
 
     }
 
