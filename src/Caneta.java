e1184d5e60c0118a20f0275d07f054ae8c727da2
Ytalo <ytalo986.x@gmail.com>
2024-10-30 09:56:46 -0300

    Primeiro codigo em java

--- src/Caneta.java
+++ src/Caneta.java
@@ -1,11 +1,11 @@
 public class Caneta {
-    String modelo;
-    String cor;
-    float ponta;
-    int carga;
-    boolean tampada;
+    public String modelo;
+    public String cor;
+    private float ponta;
+    protected int carga;
+    private boolean tampada;
 
-    void status() {
+    public void status() {
         System.out.println("Modelo: " + this.modelo);
         System.out.println("Uma caneta " + this.cor);
         System.out.println("Carga: " + this.carga);
@@ -13,7 +13,7 @@ public class Caneta {
         System.out.println("Ponta: " + this.ponta);
     }
 
-    void rabiscar() {
+    public void rabiscar() {
         if (this.tampada == true){
             System.out.println("Erro! a caneta esta tampada");
         } else {
@@ -22,13 +22,14 @@ public class Caneta {
 
     }
 
-    void tampar() {
+    public void tampar() {
         this.tampada = true;
     }
 
-    void destampar() {
+    public void destampar() {
         this.tampada = false;
     }
+
 
 
 }
