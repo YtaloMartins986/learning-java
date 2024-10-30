dfbeb5a679894f3b07a27b668d2c1164c43ce385
Ytalo <ytalo986.x@gmail.com>
2024-10-30 17:10:55 -0300

    Caneta exercicio

--- src/Caneta.java
+++ src/Caneta.java
@@ -1,17 +1,19 @@
 public class Caneta {
-    public String modelo;
-    public String cor;
+    private String modelo;
+    private String cor;
     private float ponta;
-    protected int carga;
+    private int carga;
     private boolean tampada;
 
     public void status() {
-        System.out.println("Modelo: " + this.modelo);
-        System.out.println("Uma caneta " + this.cor);
-        System.out.println("Carga: " + this.carga);
+        System.out.println("Modelo: " + this.getModelo());
+        System.out.println("Uma caneta: " + this.getCor());
+        System.out.println("Carga: " + this.getCarga());
         System.out.println("Esta tampada?: " + this.tampada);
-        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Ponta: " + this.getPonta());
     }
+
+    //Modo rabiscar
 
     public void rabiscar() {
         if (this.tampada == true){
@@ -22,6 +24,8 @@ public class Caneta {
 
     }
 
+    //Tampar/Destampar
+
     public void tampar() {
         this.tampada = true;
     }
@@ -30,6 +34,44 @@ public class Caneta {
         this.tampada = false;
     }
 
+    //Letras do modelo Protegids
 
+    public String getModelo() {
+        return this.modelo;
+    }
+
+    public void setModelo(String m) {
+        this.modelo = m;
+    }
+
+    //Valor de ponta Protegida
+
+    public float getPonta() {
+        return this.ponta;
+    }
+
+    public void setPonta(float p) {
+        this.ponta = p;
+    }
+
+    //Valor da carga protegido
+
+    public int getCarga() {
+        return this.carga;
+    }
+
+    public void setCarga(int c) {
+        this.carga = c;
+    }
+
+    //Letras de cor protegidas
+
+    public String getCor() {
+        return this.cor;
+    }
+
+    public void setCor(String c) {
+        this.cor = c;
+    }
 
 }
