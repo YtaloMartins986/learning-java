bdc08aed81d5a3253e51d7e93ae105115c2255f9
Ytalo <ytalo986.x@gmail.com>
2024-11-01 10:33:59 -0300

    Caneta exercicio

---
+++ src/NewCaneta.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class NewCaneta {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.setModelo("Attack shark R1");
+        c1.setPonta(0.5f);
+        c1.setCarga(50);
+        c1.setCor("Vermelha");
+        c1.status();
+        c1.destampar();
+        c1.rabiscar();
+
+
+
+
+    }
+}
