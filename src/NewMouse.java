2e0b3e6968cec4ca978dada226db23d205493076
Ytalo <ytalo986.x@gmail.com>
2024-11-01 10:35:13 -0300

    funcionamento Mouse [Refactor]

---
+++ src/NewMouse.java
@@ -0,0 +1,28 @@
+public class NewMouse {
+    public static void main(String[] args) {
+
+        Mouse m1 = new Mouse();
+        m1.setModelo("Attack Shark R1");
+        m1.setCor("Vermelho");
+        m1.setDpi(1200);
+        m1.setCarga(4);
+        m1.funcionando();
+        m1.status();
+        m1.movimentar();
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
