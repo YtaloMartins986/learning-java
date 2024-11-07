2c258ce578825740881bced2010cd89a2d283fd7
Ytalo <ytalo986.x@gmail.com>
2024-11-07 09:37:25 -0300

    Exercicio banco [Refactor]

--- src/Banco.java
+++ src/Banco.java
@@ -11,13 +11,14 @@ public class Banco {
             System.out.println("Tipo de conta: " + this.getTipo());
             System.out.println("Usuario da conta: " + this.getDono());
             System.out.println("Seu saldo: " + this.getSaldo());
+            System.out.println(" ");
 
     }
 
-    //Metodo Construtor contasInic
+    //Metodo Construtor para contas iniciais
     public Banco() {
-        this.saldo = 0;
-        this.status = false;
+        this.setSaldo(0);
+        this.setStatus(false);
     }
 
     //Getters e Setters
@@ -61,28 +62,28 @@ public class Banco {
 
     //Metodo tipo de conta
     public void abrirConta() {
-        if (this.tipo == "CC" && this.status == true) {
+        if (this.getTipo().equals("CC") && this.getStatus() == true) {
             this.setSaldo(50);
-        } else if (this.tipo == "CP" && this.status == true) {
+        } else if (this.getTipo().equals("CP") && this.getStatus() == true) {
             this.setSaldo(150);
         }
     }
 
     //Metodo Fechar conta
     public void fecharConta() {
-        if (this.saldo > 0.0) {
+        if (this.getSaldo() > 0.0) {
             System.out.println("Para fechar sua conta saque seu saldo.");
         } else {
-            System.out.println("Conta fechada");
             this.setStatus(false);
+            System.out.println("Conta fechada");
         }
 
     }
 
     // Metodo Deposito
     public void deposito(int v) {
-        if (this.status == true) {
-            this.setSaldo(this.saldo + v);
+        if (this.getStatus() == true) {
+            this.setSaldo(this.getSaldo() + v);
             System.out.println("Voce depositou " + v + " !");
         } else {
             System.out.println("Ative o status do seu banco para depositar");
@@ -93,7 +94,7 @@ public class Banco {
     int valorerro;
     public void sacar(int s) {
         if (s <= getSaldo()) {
-           this.setSaldo(this.saldo - s);
+           this.setSaldo(this.getSaldo() - s);
             System.out.println("Voce sacou R$" + s);
         }  else {
             this.valorerro = s;
@@ -104,17 +105,17 @@ public class Banco {
     //Metodo saqueerro
     public void saqueErro() {
         if(this.valorerro > getSaldo()) {
-            System.out.println("Voce nao tem esse valor de R$" + this.valorerro );
+            System.out.println("Saldo insuficiente! Voce nao tem esse valor de R$" + this.valorerro);
         }
 
     }
 
     // Metodo pagarMensal
     public void pagarMensal() {
-        if(getTipo() == "CC" && this.status == true) {
+        if(getTipo().equals("CC") && this.getStatus() == true) {
             this.setSaldo(this.saldo - 12);
             System.out.println(" Mensalidade paga -12R$ ");
-        } else if (getTipo() == "CP" && this.status == true){
+        } else if (getTipo().equals("CP") && this.getStatus() == true){
             this.setSaldo(this.saldo - 20);
             System.out.println(" Mensalidade paga -20R$ ");
         }
